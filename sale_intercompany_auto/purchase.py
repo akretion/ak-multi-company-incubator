cc9fca2d11a458a10e68000c716a1d05b749fb59
sebastien beau <sebastien.beau@akretion.com.br>
2012-08-21 00:30:26 +0200

    [REF] sale_intercompany_auto : refactor code in order to override it simply

--- sale_intercompany_auto/purchase.py
+++ sale_intercompany_auto/purchase.py
@@ -4,6 +4,7 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2012 Akretion
 #    author Raphaël Valyi
+#           Sébastien Beau
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -28,9 +29,37 @@ class purchase_order(osv.osv):
 
     _inherit = "purchase.order"
 
+    def _prepare_linked_sale_order(self, cr, uid, po, shop_id, context=None):
+        sale_obj = self.pool.get("sale.order")
+        vals = {'partner_id': po.company_id.partner_id.id}
+        vals.update(sale_obj.onchange_partner_id(cr, uid, [], vals['partner_id'])['value'])
+        vals.update({
+                'shop_id': shop_id,
+                'origin': 'PO:%s' % str(po.name),
+                'purchase_id': po.id,
+                'partner_shipping_id': po.dest_address_id.id #manual or automatic drop shipping
+            })
+        return vals
+
+    def _prepare_linked_sale_order_line(self, cr, uid, po_line, so_vals, context=None):
+        sale_line_obj = self.pool.get("sale.order.line")
+        vals = {'product_id': po_line.product_id.id}
+        on_change_vals = sale_line_obj.product_id_change(cr, uid, [], so_vals['pricelist_id'], 
+                                po_line.product_id.id, po_line.product_qty, po_line.product_uom.id,
+                                False, False, False, so_vals['partner_id'], False, True, False,
+                                False, False, False, context)
+        vals.update(on_change_vals['value'])
+        vals.update({
+            'tax_id': [(6, 0, on_change_vals['value']['tax_id'])],
+            'price_unit': po_line.price_unit,
+            'product_uom_qty': po_line.product_qty,
+            'product_uom': po_line.product_uom.id,
+            'purchase_line_id': po_line.id,
+        })
+        return vals
+
     def wkf_confirm_order(self, cr, uid, ids, context=None):
         sale_obj = self.pool.get("sale.order")
-        sale_line_obj = self.pool.get("sale.order.line")
         purchase_line_obj = self.pool.get("purchase.order.line")
         company_obj = self.pool.get("res.company")
         shop_obj = self.pool.get("sale.shop")
@@ -40,32 +69,17 @@ class purchase_order(osv.osv):
             partner_id = po.partner_id.id
             comp_ids = self.pool.get('res.company').search(cr, uid, [('partner_id', '=', partner_id)])
             if comp_ids:
-                so_vals = {'partner_id': po.company_id.partner_id.id}
-                so_vals.update(sale_obj.onchange_partner_id(cr, uid, [], so_vals['partner_id'])['value'])
                 comp_id = comp_ids[0]
                 shop_ids = shop_obj.search(cr, uid, [('company_id', '=', comp_id)])
                 if shop_ids:
-                    shop_id = shop_ids[0]
-                    so_vals['shop_id'] = shop_id
-                    so_vals['origin'] = 'PO:%s' % str(po.name)
-                    so_vals['purchase_id'] = po.id
-                    so_vals['partner_shipping_id'] = po.dest_address_id.id #manual or automatic drop shipping
-                    po_line_dict = {}
+                    so_vals = self._prepare_linked_sale_order(cr, uid, po, shop_ids[0], context=context)
                     order_line = []
                     for po_line in po.order_line:
-                        line_vals = {'product_id': po_line.product_id.id}
-                        #TODO extract in overridable method
-                        on_change_vals = sale_line_obj.product_id_change(cr, uid, [], so_vals['pricelist_id'], po_line.product_id.id, po_line.product_qty, po_line.product_uom.id, False, False, False, so_vals['partner_id'], False, True, False, False, False, False, context)
-                        line_vals.update(on_change_vals['value'])
-                        line_vals['tax_id'] = [(6, 0, on_change_vals['value']['tax_id'])]
-                        line_vals['price_unit'] = po_line.price_unit
-                        line_vals['product_uom_qty'] = po_line.product_qty
-                        line_vals['product_uom'] = po_line.product_uom.id
-                        line_vals['purchase_line_id'] = po_line.id
-                        order_line.append((0, 0, line_vals))
+                        order_line.append((0, 0, self._prepare_linked_sale_order_line(
+                                                    cr, uid, po_line, so_vals, context=context)
+                                            ))
                     so_vals['order_line'] = order_line
                     so_id = sale_obj.create(cr, uid, so_vals, context)
-                            
                     wf_service = netsvc.LocalService("workflow")
                     wf_service.trg_validate(uid, 'sale.order', so_id, 'order_confirm', cr) #TODO optional
         return res
